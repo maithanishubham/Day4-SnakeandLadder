1c1056590f09fe91948636ce24b904e6c096c651
shubham maithani <shubhammaithani1@gmail.com>
2022-09-14 07:22:54 -0700

    check for random option

--- snakeladder.java
+++ snakeladder.java
@@ -3,13 +3,30 @@ package com.bridgelabz.day4;
 public class snakeladder {
 
 	static int START_POSITION = 0;
+	static int NO_PLAY = 0;
+	static int IF_LADDER = 1;
+	static int IF_SNAKE = 2;
+	static int Playerposition = 0;
 
 	public static void main(String[] args) {
 
+		System.out.println("Welcome to Snake and ladder game");
+		
 		int dice = (int) (Math.random() * 10) % 6 + 1;
+		int option = (int) (Math.random() * 10) % 3;
 
-		System.out.println("Welcome to Snake and ladder Game");
-		System.out.println("rolling of the die Number:" + dice);
+		if (option == IF_LADDER) {
+			System.out.println("Ladder!!");
+			Playerposition += dice;
+
+		} else if (option == IF_SNAKE) {
+			System.out.println("snake!!");
+			Playerposition -= dice;
+
+		} else {
+			System.out.println("N0 play");
+		}
+
 	}
 
 }
